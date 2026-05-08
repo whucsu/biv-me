750be53965ca25c0cd5268bee76486bbbeb47601
Joshua Dillon <jdil469@aucklanduni.ac.nz>
2026-05-08 20:04:36 +1200

    Add argument to main.py to run individual cases

--- src/bivme/main.py
+++ src/bivme/main.py
@@ -122,6 +122,29 @@ def validate_config_fitting(config, mylogger):
         mylogger.error(f'argument workers must be a positive integer. {config["multiprocessing"]["workers"]} given.')
         sys.exit(0)
 
+def validate_config_misc(config, mylogger):
+    # Validate logging parameters
+    if not (config["logging"]["show_detailed_logging"] == True or config["logging"]["show_detailed_logging"] == False):
+        mylogger.error(f'Invalid logging option: {config["logging"]["show_detailed_logging"]}. Must be True or False.')
+        sys.exit(0)
+    if not (config["logging"]["generate_log_file"] == True or config["logging"]["generate_log_file"] == False):
+        mylogger.error(f'Invalid logging option: {config["logging"]["generate_log_file"]}. Must be True or False.')
+        sys.exit(0)
+
+    # Validate plotting parameters
+    if not (config["plotting"]["generate_plots_preprocessing"] == True or config["plotting"]["generate_plots_preprocessing"] == False):
+        mylogger.error(f'Invalid plotting option: {config["plotting"]["generate_plots_preprocessing"]}. Must be True or False.')
+        sys.exit(0)
+    if not (config["plotting"]["generate_plots_fitting"] == True or config["plotting"]["generate_plots_fitting"] == False):
+        mylogger.error(f'Invalid plotting option: {config["plotting"]["generate_plots_fitting"]}. Must be True or False.')
+        sys.exit(0)
+    if not (config["plotting"]["include_images"] == True or config["plotting"]["include_images"] == False):
+        mylogger.error(f'Invalid plotting option: {config["plotting"]["include_images"]}. Must be True or False.')
+        sys.exit(0)
+    if not (config["plotting"]["export_images"] == True or config["plotting"]["export_images"] == False):
+        mylogger.error(f'Invalid plotting option: {config["plotting"]["export_images"]}. Must be True or False.')
+        sys.exit(0)
+    
 
 def run_preprocessing(case, config, mylogger):
     try:
@@ -212,6 +235,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run biv-me modules')
     parser.add_argument('-config', '--config_file', type=str,
                         help='Config file describing which modules to run and their associated parameters', default='configs/config.toml')
+    parser.add_argument('-case', '--case_name', type=str, 
+                        help='Case name to process (only this case will be run)', default=None)
+
     args = parser.parse_args()
 
     # Set up logging
@@ -265,33 +291,21 @@ if __name__ == "__main__":
 
     logger.info(f'Running modules: preprocessing={run_preprocessing_bool}, fitting={run_fitting_bool}')
 
-    # Validate logging parameters
-    if not (config["logging"]["show_detailed_logging"] == True or config["logging"]["show_detailed_logging"] == False):
-        logger.error(f'Invalid logging option: {config["logging"]["show_detailed_logging"]}. Must be True or False.')
-        sys.exit(0)
-    if not (config["logging"]["generate_log_file"] == True or config["logging"]["generate_log_file"] == False):
-        logger.error(f'Invalid logging option: {config["logging"]["generate_log_file"]}. Must be True or False.')
-        sys.exit(0)
-
-    # Validate plotting parameters
-    if not (config["plotting"]["generate_plots_preprocessing"] == True or config["plotting"]["generate_plots_preprocessing"] == False):
-        logger.error(f'Invalid plotting option: {config["plotting"]["generate_plots_preprocessing"]}. Must be True or False.')
-        sys.exit(0)
-    if not (config["plotting"]["generate_plots_fitting"] == True or config["plotting"]["generate_plots_fitting"] == False):
-        logger.error(f'Invalid plotting option: {config["plotting"]["generate_plots_fitting"]}. Must be True or False.')
-        sys.exit(0)
-    if not (config["plotting"]["include_images"] == True or config["plotting"]["include_images"] == False):
-        logger.error(f'Invalid plotting option: {config["plotting"]["include_images"]}. Must be True or False.')
-        sys.exit(0)
-    if not (config["plotting"]["export_images"] == True or config["plotting"]["export_images"] == False):
-        logger.error(f'Invalid plotting option: {config["plotting"]["export_images"]}. Must be True or False.')
-        sys.exit(0)
+    # Validate logging and plotting parameters
+    validate_config_misc(config, logger)
     
     # Determine which cases to process
     if run_preprocessing_bool: # then get case list from preprocessing source directory
         validate_config_preprocessing(config, logger)
         caselist = os.listdir(config["input_pp"]["source"])
         caselist = [case for case in caselist if os.path.isdir(os.path.join(config["input_pp"]["source"], case))]
+
+        if args.case_name is not None:
+            if args.case_name in caselist:
+                caselist = [args.case_name]
+            else:
+                logger.error(f'Case {args.case_name} not found in source directory {config["input_pp"]["source"]}.')
+                sys.exit(0)
 
         # Edit gp_directory to point to the output of the preprocessing
         gp_dir = os.path.join(config["output_pp"]["output_directory"], config["input_pp"]["batch_ID"])
@@ -306,6 +320,13 @@ if __name__ == "__main__":
         if not run_preprocessing_bool: # then get case list from fitting source directory
             caselist = os.listdir(config["input_fitting"]["gp_directory"])
             caselist = [case for case in caselist if os.path.isdir(os.path.join(config["input_fitting"]["gp_directory"], case))]
+
+        if args.case_name is not None:
+            if args.case_name in caselist:
+                caselist = [args.case_name]
+            else:
+                logger.error(f'Case {args.case_name} not found in gp_directory {config["input_fitting"]["gp_directory"]}.')
+                sys.exit(0)
 
         # save a copy of the config file to the output folder
         output_folder = Path(config["output_fitting"]["output_directory"])
